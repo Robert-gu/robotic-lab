b0b25d9aabebdd3c70be904d3437623f40497925
Robert-gu <69894736+Robert-gu@users.noreply.github.com>
2020-09-10 01:44:39 +1000

    Add files via upload

---
+++ Lab03_M2_Group05/Week03-05/ReadMe.txt
@@ -0,0 +1,6 @@
+Key.up Key.down Key.left Key.right move the robot forward and backward or turn it right and left.
+Due to friction please don't turn the robot left or right for too long or too short. Pressing about half a second is best.
+The robot only take one command at a time. e.g. it can't go forward and turn left at the same time.
+for calibration:
+scale=2.719239e-03
+baseline=4.8987e-01
---
+++ Lab03_M2_Group05/Week03-05/keyboardControlARtestStarter.py
@@ -0,0 +1,168 @@
+# show ARUCO marker detection annotations when teleoperating the robot through keyboard
+
+from pynput.keyboard import Key, Listener, KeyCode
+import cv2
+import numpy as np
+
+# import OpenCV ARUCO functions
+import cv2.aruco as aruco
+
+# replace with your own keyboard teleoperation codes
+class Keyboard:
+    # feel free to change the speed, or add keys to do so
+    wheel_vel_forward = 100
+    wheel_vel_rotation = 40
+
+    def __init__(self, ppi=None):
+        # storage for key presses
+        self.directions = [False for _ in range(4)]
+        self.signal_stop = False 
+        self.speed_up=False
+
+        # connection to PenguinPi robot
+        self.ppi = ppi
+        self.wheel_vels = [0, 0]
+
+        self.listener = Listener(on_press=self.on_press, on_release=self.on_release).start()
+
+    def on_release(self, key):
+        #print(f"released {key}")       
+        if key == Key.up:
+            self.directions[0] = False
+        elif key == Key.down:
+            self.directions[1] = False
+        elif key == Key.left:
+            self.directions[2] = False
+        elif key == Key.right:
+            self.directions[3] = False
+        elif key == Key.shift:
+            self.speed_up = False
+
+        self.send_drive_signal()
+
+    def on_press(self, key):
+        #print(f"pressed {key}")       
+        # use arrow keys to drive, space key to stop
+        # feel free to add more keys
+
+        if key == Key.up:
+            self.directions[0] = True
+        elif key == Key.down:
+            self.directions[1] = True
+        elif key == Key.left:
+            self.directions[2] = True
+        elif key == Key.right:
+            self.directions[3] = True
+        #elif key == Key.space:
+        #   self.signal_stop = True
+        elif key == Key.shift:
+            self.speed_up = True
+        
+        self.send_drive_signal()
+
+    def get_drive_signal(self):           
+        # translate the key presses into drive signals 
+        speed = Keyboard.wheel_vel_forward
+        speed_increase = 100
+        turning_speed = Keyboard.wheel_vel_rotation
+
+        #move forward 
+        left_speed = 0
+        right_speed = 0
+
+        if self.speed_up == True:
+            speed= Keyboard.wheel_vel_forward+speed_increase;
+        else:
+            speed= Keyboard.wheel_vel_forward;
+        
+        if self.directions[0] == True : #forward
+            left_speed = speed;
+            right_speed = speed;
+
+        elif self.directions[1] == True : #backwards
+            left_speed = -speed;
+            right_speed = -speed;
+
+        elif self.directions[2] == True: # Left
+            left_speed = -turning_speed;
+            right_speed = turning_speed;
+
+        elif self.directions[3] ==True: # Right
+            left_speed = turning_speed;
+            right_speed = -turning_speed;
+
+        #print(self.directions)
+        '''elif self.signal_stop == True:
+            left_speed = 0
+            right_speed = 0
+
+        self.signal_stop = False
+
+        # reset directions array
+        for i in range(len(self.directions)):
+            self.directions[i] = False;'''
+
+        return left_speed, right_speed
+    
+    def send_drive_signal(self):
+        if not self.ppi is None:
+            lv, rv = self.get_drive_signal()
+            lv, rv = self.ppi.set_velocity(lv, rv)
+            self.wheel_vels = [lv, rv]
+            
+    def latest_drive_signal(self):
+        return self.wheel_vels
+    
+
+if __name__ == "__main__":
+    import penguinPiC
+    ppi = penguinPiC.PenguinPi()
+
+    keyboard_control = Keyboard(ppi)
+
+    cv2.namedWindow('video', cv2.WINDOW_NORMAL);
+    cv2.setWindowProperty('video', cv2.WND_PROP_AUTOSIZE, cv2.WINDOW_AUTOSIZE);
+
+    while True:
+        # font display options
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        location = (0, 0)
+        font_scale = 1
+        font_col = (255, 255, 255)
+        line_type = 2
+
+        # Get velocity of each wheel
+        wheel_vels = keyboard_control.latest_drive_signal();
+        L_Wvel = wheel_vels[0]
+        R_Wvel = wheel_vels[1]
+
+        # Get current frame
+        curr = ppi.get_image()
+        
+        # uncomment to see how noises influence the accuracy of ARUCO marker detection
+        #im = np.zeros(np.shape(curr), np.uint8)
+        #cv2.randn(im,(0),(99))
+        #curr = curr + im
+        
+        # show ARUCO marker detection annotations
+        aruco_params = aruco.DetectorParameters_create()
+        aruco_params.minDistanceToBorder = 0
+        aruco_params.adaptiveThreshWinSizeMax = 1000
+        aruco_dict = aruco.Dictionary_get(cv2.aruco.DICT_4X4_100)
+    
+        corners, ids, rejected = aruco.detectMarkers(curr, aruco_dict, parameters=aruco_params)
+    
+        aruco.drawDetectedMarkers(curr, corners, ids) # for detected markers show their ids
+        aruco.drawDetectedMarkers(curr, rejected, borderColor=(100, 0, 240)) # unknown squares
+        
+        # Scale to 144p
+        resized = cv2.resize(curr, (960, 720), interpolation = cv2.INTER_AREA)
+
+        # Replace with your own GUI
+        cv2.putText(resized, 'PenguinPi', (15, 50), font, font_scale, font_col, line_type)
+
+        cv2.imshow('video', resized)
+
+        cv2.waitKey(1)
+
+        continue
